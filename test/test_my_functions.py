a339994e103d0083018164d8a2f7b8774c7145b4
ShubhenduADutta <shubhenduanupam@gmail.com>
2023-10-30 18:20:13 +0530

    Pytest marking

--- test/test_my_functions.py
+++ test/test_my_functions.py
@@ -1,7 +1,7 @@
 """
 This file contains the tests for the functions in my_functions.py
 """
-
+import time
 import pytest
 from source import my_functions
 
@@ -28,3 +28,22 @@ def test_divide_by_zero():
     """Test that the divide function raises a ValueError when dividing by zero"""
     with pytest.raises(ValueError):
         my_functions.divide(1, 0)
+
+
+@pytest.mark.slowtest
+def test_very_slow():
+    """Test that the divide function works as expected, slowed down"""
+    time.sleep(5)
+    assert my_functions.divide(1, 2) == 0.5
+
+
+@pytest.mark.skip(reason="This feature is currently broken")
+def test_add_skipped():
+    """Test that the add function works as expected, but currently skipped"""
+    assert my_functions.add(1, 2) == 3
+
+
+@pytest.mark.xfail(reason="We know we can't divide by zero")
+def test_divide_zero_broken():
+    """Test that the divide function raises a ValueError when dividing by zero, example of xfail"""
+    my_functions.divide(3, 0)
